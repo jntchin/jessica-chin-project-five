f0891d4c79fa28f4c9203199b78241c46a16af9d
Jessica Chin <jntchin@gmail.com>
2019-09-05 19:18:45 -0400

    submit button working, proxy server established

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import './App.css';
-import axios from 'axios';
+// import axios from 'axios';
+// import Qs from 'qs';
 import GrammarForm from './GrammarForm';
-import Button from './Button';
+// import Button from './Button';
 
 class App extends Component {
   constructor(){
@@ -14,42 +15,33 @@ class App extends Component {
     }
   }
 
-  checkMyGrammar = (userInput) => {
-    axios ({
-      method: 'GET',
-      url: 'http://proxy.hackeryou.com',
-      
-      dataResponse: 'JSON',
-      params: {
-        reqUrl: 'http://api.grammarbot.io/v2/check?',
-        paramsSerializer: function(params) {
-          return Qs.stringify(params, {arrayFormat: 'brackets'})
-        },
-        proxyHeaders: {
-          'header_params': 'value',
-          key: 'KS9C5N3Y',
-          format: 'JSON',
-          text: {userInput},
-          language: '=en-CA'
-        }
-      }
-    }).then((results) => {
-      this.setState({
-        edits: [results.sentence]
-      })
-    })
-  }
-
-  // handleChange = event => {
-  //   this.setState({
-  //     userInput: event.target.value
+  // checkMyGrammar = () => {
+  //   console.log(this.state)
+  //   axios ({
+  //     method: 'GET',
+  //     url: 'http://proxy.hackeryou.com',
+  //     dataResponse: 'JSON',
+  //     params: {
+  //       reqUrl: 'http://api.grammarbot.io/v2/check?',
+  //       paramsSerializer: function(params) {
+  //         return Qs.stringify(params, {arrayFormat: 'brackets'})
+  //       },
+  //       xmlToJSON: false,
+  //       proxyHeaders: {
+  //         'header_params': 'value',
+  //         key: 'KS9C5N3Y',
+  //         format: 'JSON',
+  //         text: this.state.userInput,
+  //         language: '=en-CA'
+  //       }
+  //     }
+  //   }).then((results) => {
+  //     this.setState({
+  //       edits: [results.sentence]
+  //     })
   //   })
   // }
 
-  // handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   this.checkMyGrammar();
-  // }
     
 
   render(){
@@ -58,7 +50,7 @@ class App extends Component {
         <h1>Grammarist</h1>
         <p>Type a sentence below</p>
         <GrammarForm grammarFunction={this.checkMyGrammar} myName='Jess' />
-        <Button />
+        {/* <Button /> */}
         <button>Hide my shame</button>
         <div>
           <p></p>
--- src/GrammarForm.js
+++ src/GrammarForm.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import axios from 'axios';
+import Qs from 'qs';
 
 export default class GrammarForm extends React.Component {
+
+
 
     constructor(){
         super();
@@ -11,6 +15,35 @@ export default class GrammarForm extends React.Component {
         }
       }
 
+
+      checkMyGrammar = () => {
+        console.log(this.state)
+        axios ({
+          method: 'GET',
+          url: 'http://proxy.hackeryou.com',
+          dataResponse: 'JSONP',
+          params: {
+            reqUrl: 'http://api.grammarbot.io/',
+            paramsSerializer: function(params) {
+              return Qs.stringify(params, {arrayFormat: 'brackets'})
+            },
+            xmlToJSON: false,
+            proxyHeaders: {
+              'header_params': 'value',
+              key: 'KS9C5N3Y',
+              format: 'JSON',
+              text: this.state.userInput,
+              language: 'en-CA'
+            }
+          }
+        }).then((results) => {
+        //   this.setState({
+        //     edits: [results.sentence]
+        //   })
+        console.log(results);
+        })
+      }
+
     handleChange = event => {
         this.setState({
           userInput: event.target.value,
@@ -19,18 +52,20 @@ export default class GrammarForm extends React.Component {
     
       handleSubmit = (event) => {
         event.preventDefault();
-        this.props.grammarFunction();
+        this.checkMyGrammar();
+        console.log('handleSubmit')
       }
 
     render(){
         return(
-            <form action="" onSubmit={this.handleSubmit}>
+            <form action="">
                 <p>{this.props.myName}</p>
                 <input 
                     type="text" 
                     onChange={this.handleChange}
                     value={this.state.userInput}
                 />
+                <button onClick={this.handleSubmit} type="submit">Check my grammar!</button>
             </form>
         )
     }
