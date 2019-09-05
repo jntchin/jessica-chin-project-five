cb89a392daabebb24aba03caf7d1ad726fda5f88
Jessica Chin <jntchin@gmail.com>
2019-09-05 12:46:34 -0400

    first commit for real this time

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      priorEdits: [],
+    }
+  }
+
+  componentDidMount (){
+    //make AJAX request
+    axios ({
+      method: 'GET',
+      url: 'http://api.grammarbot.io/v2/check?api_key=KS9C5N3Y&text=&language=en-CA',
+      dataResponse: 'JSON',
+      params: {
+        key: 'KS9C5N3Y',
+        format: 'JSON'
+      }
+    }).then((results) =>{
+      console.log('sup');
+      results = results.data.sentences
+      this.setState({
+        priorEdits: results,
+      })
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Grammarist</h1>
+        <p>Type a sentence below</p>
+        <form action="">
+          <input type="text"/>
+        </form>
+        <button>Check my grammar!</button>
+        <button>Hide my shame</button>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
