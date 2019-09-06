7b9e55946b56f900fb0c3e9cae6bf26aeb06a53b
Jessica Chin <jntchin@gmail.com>
2019-09-06 17:01:24 -0400

    need to map over array to get exact results to then print to page. but maybe refactor later

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
+import Qs from 'qs';
 import GrammarForm from './GrammarForm';
 // import firebase from './firebase';
 
@@ -10,8 +12,56 @@ class App extends Component {
 
     this.state = {
       userInput: '',
-      edits: '',
+      edits: [],
     }
+  }
+
+  checkMyGrammar = () => {
+    console.log(this.state)
+    axios ({
+      method: 'GET',
+      url: 'http://proxy.hackeryou.com',
+      dataResponse: 'JSON',
+      paramsSerializer: function(params) {
+        return Qs.stringify(params, {arrayFormat: 'brackets'})
+      },
+      params: {
+        reqUrl: 'http://api.grammarbot.io/v2/check',
+        params: {
+            queryParam: this.state.userInput,
+            key: 'KS9C5N3Y',
+            format: 'JSON',
+            text: this.state.userInput,
+            language: 'en-CA',
+        }, 
+        proxyHeaders: {
+          'header_params': 'value',
+        },
+        xmlToJSON: false,
+      }
+    }).then((results) => {
+      console.log(results);
+      this.setState({
+        edits: results.data.matches,
+
+        //map over this array of edits (has to go deeper)
+        //right now it's an array of objects, but you want the string
+      });
+    })
+  }
+
+  
+
+  handleChange = (event) => {
+    this.setState({
+      userInput: event.target.value,
+    }) 
+  }
+
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.checkMyGrammar();
   }
 
   render(){
@@ -19,9 +69,9 @@ class App extends Component {
       <div className="App">
         <h1>Grammarist</h1>
         <p>Type a sentence below</p>
-        <GrammarForm grammarFunction={this.checkMyGrammar} myName='Jess' />
+        <GrammarForm run={this.checkMyGrammar} handleChange = {this.handleChange} handleSubmit={this.handleSubmit} myName='Jess'/>
         <div>
-          <p></p>
+          <p>{this.state.userInput}</p>
         </div>
       </div>
     );
--- src/GrammarForm.js
+++ src/GrammarForm.js
@@ -1,65 +1,15 @@
 import React from 'react';
-import axios from 'axios';
-import Qs from 'qs';
+
 
 export default class GrammarForm extends React.Component {
     
-    constructor(){
-        super();
-    
-        this.state = {
-          userInput: '',
-          errors: '',
-        }
-      }
-
-
-      checkMyGrammar = () => {
-        console.log(this.state)
-        axios ({
-          method: 'GET',
-          url: 'http://proxy.hackeryou.com',
-          dataResponse: 'JSON',
-          paramsSerializer: function(params) {
-            return Qs.stringify(params, {arrayFormat: 'brackets'})
-          },
-          params: {
-            reqUrl: 'http://api.grammarbot.io/v2/check',
-            params: {
-                queryParam: this.state.userInput,
-                key: 'KS9C5N3Y',
-                format: 'JSON',
-                text: this.state.userInput,
-                language: 'en-CA',
-            }, 
-            proxyHeaders: {
-              'header_params': 'value',
-            },
-            xmlToJSON: false,
-          }
-        }).then((results) => {
-          console.log(results);
-          const edits = results.data.matches;
-          console.log('this is the edits variable', edits);
-          //take the results and pull out the results. Store that in a variable called edits
-          // const edits = [];
-          //replace this console log with a way to print this to the page. Does it need to be here or at the handleSubmit stage??
-          // console.log(edits.userInput, edits.errors)
-        })
-      }
-
-    handleChange = event => {
-        this.setState({
-          userInput: event.target.value,
-          errors: event.results,
-        })
-        
-      }
-    
-      handleSubmit = (event) => {
-        event.preventDefault();
-        this.checkMyGrammar();
-      }
+    // constructor(){
+    //     super();    
+    //     this.state = {
+    //       userInput: '',
+    //       errors: '',
+    //     }
+    //   }
 
     render(){
         return(
@@ -67,15 +17,20 @@ export default class GrammarForm extends React.Component {
                 <p>{this.props.myName}</p>
                 <input 
                     type="text" 
-                    onChange={this.handleChange}
-                    value={this.state.userInput}
+                    onChange={this.props.handleChange}
+                    value={this.props.userInput}
                 />
-                <button onClick={this.handleSubmit} type="submit">Check my grammar!</button>
-                <p></p>
-                <button variant="primary" size="large">Hide my shame</button>
+                <button onClick={this.props.handleSubmit} type="submit">Check my grammar!</button>
+                <button>Hide my shame</button>
             </form>
         )
     }
 
 
 }
+
+
+          //take the results and pull out the results. Store that in a variable called edits
+          // const edits = [];
+          //replace this console log with a way to print this to the page. Does it need to be here or at the handleSubmit stage??
+          // console.log(edits.userInput, edits.errors)
