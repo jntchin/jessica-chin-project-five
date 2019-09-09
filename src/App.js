7fa24a3355d9cba5e98c88f66cdfa3dda45913f9
Jessica Chin <jntchin@gmail.com>
2019-09-09 18:54:15 -0400

    getting frustrated trying to add error message when user types a grammatically correct sentence

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import axios from 'axios';
 import Qs from 'qs';
 import GrammarForm from './GrammarForm';
 import ResetButton from './ResetButton';
-import firebase from './firebase';
+// import firebase from './firebase';
 
 
-const dbRef = firebase.database().ref();
+// const dbRef = firebase.database().ref();
 
 
 
@@ -22,13 +22,12 @@ class App extends Component {
       savedInput: '',
       edits: [],
       charsLeft: 50000,
+      perfectSentence: '',
     }
   }
 
-
   //axios call
   checkMyGrammar = () => {
-    console.log(this.state)
     axios ({
       method: 'GET',
       url: 'http://proxy.hackeryou.com',
@@ -54,7 +53,13 @@ class App extends Component {
       this.setState({
         edits: results.data.matches
       })
-    })
+      if (results.data.matches === []){
+        this.setState({
+          perfectSentence: 'You wrote a perfect sentence',
+        })
+      } 
+    }) 
+    console.log(this.state);
   }
 
   //when the user types in the form
@@ -88,6 +93,8 @@ class App extends Component {
     }
   }
 
+  
+
   //functions for the "hide my shame" button
   reset(){
     this.setState({
@@ -103,55 +110,61 @@ class App extends Component {
   }
 
   characterCountMessage = () => {
-      //if charCount is 50,000 return go ahead and write something, don't print userInput
+      //if the number of characters left is 50,000 return go ahead and write something, don't print userInput
     if (this.state.charsLeft === 50000) {
-      return 'Go ahead and write something'
-    //if charCount is 49,975-49,999 print must be at least 25 characters
+      return 'Go ahead and write something!'
+    //if the number of characters left is 49,975-49,999 print must be at least 25 characters
     } else if (this.state.charsLeft <= 49999 && this.state.charsLeft > 49975){
       return 'Must be at least 25 characters'
-      //if charCount is 49,974 or less, print the userInput
+      //if the number of characters left is 49,974 or less, print the userInput
     } else if (this.state.charsLeft <= 49975){
       return null 
-      //if charCount is 0, write limit of 50,000 characters, don't render userInput
+      //if the number of characters left is 0, write limit of 50,000 characters, don't render userInput
     } else if (this.state.charsLeft === 0){
-      return (`You've hit your character limit`)
+      return (`You've hit the character limit!`)
     }
   }
 
  
   render(){
+    // const perfectSentence = 'You wrote a perfect sentence! Good job!'
     return (
       <div className="App">
-        <h1>Grammarist</h1>
-        <h2>Let me tell you what <span>you're</span> your problem is!</h2>
-        <p>Type a sentence below.</p>
+        <header>
+          <h1>Grammarist</h1>
+          <h2>Let me tell you what <span>you're</span> your problem is!</h2>
+          <p>Type a sentence below.</p>
+        </header>
+        
+        {/* run the axios call in the form component */}
         <GrammarForm run={this.checkMyGrammar} handleChange = {this.handleChange} handleSubmit={this.handleSubmit} inputField={this.state.inputField} charsLeft={this.state.charsLeft} userInput={this.state.userInput} />
+
         <div className="errors"> 
-          
+           {/* if the user writes a perfect sentence */}
+    
+          {/* add error handling messages if necessary */}
           <p className="errorHandling">{this.characterCountMessage()}</p>
-          
+
+          {/* map over the axios call results to find the error messages */}
           <ul>{this.state.edits.map((errorMessages, index) =>{
             return(
               <li key={index}>
-                <p>{this.state.savedInput}</p>
-                <p className="shortMessage">{errorMessages.shortMessage}</p>
+                <p className="savedInput">{this.state.savedInput}</p>
+                {/* <p className="shortMessage">{errorMessages.shortMessage}</p> */}
                 <p className="message">{errorMessages.message}</p>
               </li>
               ) 
             })}
           </ul>
+          
         </div>
+        
+        {/* reset the form */}
         <ResetButton handleReset={this.handleReset} />
+
       </div>
     );
   }
 }
 
 export default App;
-
-
-
-
-
- {/* 
-{(errorMessages.shortMessage = "") ? null : <span className="shortMessageSpan">You've got a</span>}{errorMessages.shortMessage} */}
--- src/GrammarForm.js
+++ src/GrammarForm.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 export default class GrammarForm extends React.Component {
     render(){
         return(
@@ -12,15 +11,9 @@ export default class GrammarForm extends React.Component {
                         value={this.props.inputField}
                     >
                     </textarea>
-                    <button onClick={this.props.handleSubmit} disabled={this.props.charsLeft >= 49975 ? "disabled" : null}  type="submit">Check my grammar!</button>
+                    <button onClick={this.props.handleSubmit} disabled={this.props.charsLeft > 49975 ? "disabled" : null}  type="submit">Check my grammar!</button>
                 </form>
             </div>
         )
     }
 }
-
-
-          //take the results and pull out the results. Store that in a variable called edits
-          // const edits = [];
-          //replace this console log with a way to print this to the page. Does it need to be here or at the handleSubmit stage??
-          // console.log(edits.userInput, edits.errors)
