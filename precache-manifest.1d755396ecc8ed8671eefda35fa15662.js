a31b49c6c01311a4769506de90f15d55b957d7bc
Jessica Chin <jntchin@gmail.com>
2019-09-24 16:18:46 -0400

    Updates

--- precache-manifest.1d755396ecc8ed8671eefda35fa15662.js
+++ precache-manifest.1d755396ecc8ed8671eefda35fa15662.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "241f9a4721e49b12e1007de61e68a9f6",
+    "revision": "b994bb432c8526f79d3d7a37025deac8",
     "url": "/jessica-chin-project-five/index.html"
   },
   {
-    "revision": "78dabcb8c93408096a03",
-    "url": "/jessica-chin-project-five/static/css/main.e9ed34d4.chunk.css"
+    "revision": "9ed91f807fe146888711",
+    "url": "/jessica-chin-project-five/static/css/main.e4ddbac6.chunk.css"
   },
   {
     "revision": "871335bfef1bcd614854",
     "url": "/jessica-chin-project-five/static/js/2.5adb0bfb.chunk.js"
   },
   {
-    "revision": "78dabcb8c93408096a03",
-    "url": "/jessica-chin-project-five/static/js/main.c7efb36f.chunk.js"
+    "revision": "9ed91f807fe146888711",
+    "url": "/jessica-chin-project-five/static/js/main.cb3e567d.chunk.js"
   },
   {
     "revision": "07c0339a32f91f8e114f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jessica-chin-project-five/precache-manifest.1cb5703edefa32a2c77e4082633dc6a0.js"
+  "/jessica-chin-project-five/precache-manifest.1d755396ecc8ed8671eefda35fa15662.js"
 );
 
 self.addEventListener('message', (event) => {
